41648e4301b6d2f31a99c7d57df5a0a7f6d3fc0d
cauematheus23 <cauematheus_23@hotmail.com>
2024-05-05 01:34:44 -0300

    commit

---
+++ 5by5-Idade_Em_Bissextos/Pessoa.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _5by5_Idade_Em_Bissextos
+{
+    internal class Pessoa
+    {
+        string name;
+        int age, leapyears = 0;
+        string job;
+
+        public Pessoa(string name, int age, string job)
+        {
+            this.name = name;
+            this.age = age;
+            this.job = job;
+        }
+        public void LeapYears()
+        {
+            int year = 2024 - age;
+            
+            while (year <= 2024) 
+            {
+                if (DateTime.IsLeapYear(year))
+                {
+                    leapyears++;
+                }
+                year++;
+            }
+         }
+        
+        public void Print()
+        {
+            Console.WriteLine("Name: " + this.name);
+            Console.WriteLine("Age: " + this.age);
+            Console.WriteLine("Job: " + this.job);
+            Console.WriteLine("Leap years: " + this.leapyears);
+        }
+
+    }
+}
--- 5by5-Idade_Em_Bissextos/Program.cs
+++ 5by5-Idade_Em_Bissextos/Program.cs
@@ -1,1 +1,14 @@
-﻿
+﻿using _5by5_Idade_Em_Bissextos;
+
+string name, job;
+int age;
+Console.WriteLine("Write your name: ");
+name = Console.ReadLine();
+Console.WriteLine("Write your job: ");
+job = Console.ReadLine();
+Console.WriteLine("Write your age: ");
+age = int.Parse(Console.ReadLine());
+
+Pessoa pessoa = new Pessoa(name, age,job);
+pessoa.LeapYears();
+pessoa.Print();
